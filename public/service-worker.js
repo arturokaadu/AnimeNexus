90e6ad2b4445e93edb6e88e81fd3f50bb692dcd2
arturokaadu <arturokaadu@gmail.com>
2026-01-14 14:52:51 -0300

    fix: Add cleanup service-worker.js to unregister cached SW

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+// This service worker file immediately unregisters itself to clean up any cached workers
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        self.registration.unregister().then(function () {
+            return self.clients.matchAll();
+        }).then(function (clients) {
+            clients.forEach(client => client.navigate(client.url));
+        })
+    );
+});
